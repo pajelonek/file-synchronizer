35ce87d877bb72df1dffcf16b35eee7898d6b04e
pajelonek <pawel.jelonek.krakow@gmail.com>
2020-02-27 19:55:19 +0100

    Initial commit

---
+++ src/test/java/com/licencjat/filesynchronizer/FileSynchronizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licencjat.filesynchronizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSynchronizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
